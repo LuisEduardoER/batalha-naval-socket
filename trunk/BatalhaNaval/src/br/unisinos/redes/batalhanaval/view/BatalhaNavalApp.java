ddef83fcdfb7377af23715e4c78cb49e5dc34db9
rholtzbr@gmail.com <rholtzbr@gmail.com@8d5ad0dd-6379-138e-86b1-2c6862ce9929>
2011-04-13 03:22:49 +0000

    inicio

---
+++ trunk/BatalhaNaval/src/br/unisinos/redes/batalhanaval/view/BatalhaNavalApp.java
@@ -0,0 +1,44 @@
+/*
+ * BatalhaNavalApp.java
+ */
+
+package br.unisinos.redes.batalhanaval.view;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class BatalhaNavalApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new BatalhaNavalView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of BatalhaNavalApp
+     */
+    public static BatalhaNavalApp getApplication() {
+        return Application.getInstance(BatalhaNavalApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(BatalhaNavalApp.class, args);
+    }
+}
